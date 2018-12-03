cc2c681975c9a35406ae88145e0026b768ab5cfb
Martin Baulig <mabaul@microsoft.com>
2018-12-03 11:25:27 -0500

    Copy the new code over from System.Net.Http.SocketsHttpHandler.

---
+++ mcs/class/System.Net.Http/corefx/PlatformDetection.cs
@@ -0,0 +1,16 @@
+namespace System
+{
+	static partial class PlatformDetection
+	{
+		public static bool IsOSX => true;
+		public static Version OpenSslVersion => new Version (-1, 0);
+		public static bool IsDebian => false;
+		public static bool IsRedHatFamily6 => false;
+		public static bool IsMacOsHighSierraOrHigher => true;
+		public static int WindowsVersion => -1;
+		public static bool IsMono => true;
+
+		public static bool SupportsX509Chain => UsingBtls;
+		public static bool UsingBtls => string.Equals (Environment.GetEnvironmentVariable ("MONO_TLS_PROVIDER"), "btls");
+	}
+}
